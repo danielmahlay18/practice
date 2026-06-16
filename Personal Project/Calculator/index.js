1a3ee2b6e0a95208a2334bee24a00f00cb81df0f
Daniel Omale <danielmahlay18@gmail.com>
2026-06-16 13:45:59 +0100

    calculator project

---
+++ Personal Project/Calculator/index.js
@@ -0,0 +1,23 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = '';
+}
+
+function calculate(){
+ try {
+  display.value = eval(display.value);
+  console.log(display.value);  
+ } catch (error) {
+    display.value = 'ERROR';
+ }
+}
+    
+    
+    
